cbe300cba26e4b750c87c95cd8ba631977793d75
Ryota Yasuhara <c0b2314282@edu.teu.ac.jp>
2025-04-15 16:47:49 +0900

    練習10-4

--- pygame_image.py
+++ pygame_image.py
@@ -19,7 +19,17 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() #練習10-3
 
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1) #練習10-4
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1) #練習10-4
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0) #練習10-4
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0) #練習10-4
+        
         x = tmr%3200 #練習６
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600, 0]) #練習７
