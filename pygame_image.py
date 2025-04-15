18f6f661d857e7895f4910ee5fdb3efe7daa487e
Ryota Yasuhara <c0b2314282@edu.teu.ac.jp>
2025-04-15 17:23:55 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -16,7 +16,9 @@ def main():
     kk_rct = kk_img.get_rect() #練習10-1:こうかとんRectの抽出
     kk_rct.center = 300, 200 #練習10-2:こうかとんRectの中心座標を指定
     tmr = 0
+    
     while True:
+        x = tmr%3200 
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed() #練習10-3
@@ -29,8 +31,11 @@ def main():
             kk_rct.move_ip(-1, 0) #練習10-4
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip(+1, 0) #練習10-4
-        
-        x = tmr%3200 #練習６
+        else:
+            kk_rct.move_ip(-1,0) #演習1
+
+
+        #x = tmr%3200 #練習６
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600, 0]) #練習７
         screen.blit(bg_img, [-x + 3200, 0]) #練習9
